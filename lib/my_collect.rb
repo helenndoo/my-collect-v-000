b28abd55f1ae4341bd98b0876912c9bd70d92aed
Helen Du <helendu.614@gmail.com>
2018-01-06 01:51:17 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,9 +4,9 @@ def my_collect(array)
   if block_given?
     i = 0
     new_array = []
-    while i < array.count
+    while i < array.length
       new_array << yield(array[i])
-      i += 1
+      i += 1 # i = i + 1
     end
     new_array
   end
